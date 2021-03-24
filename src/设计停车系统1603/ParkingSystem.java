2fd3d95be21260c5b4c597aa830cce55c60d9c91
kai.zheng <kai.zheng@bj-tct.com>
2021-03-24 15:06:23 +0800

    add no.1603

---
+++ src/设计停车系统1603/ParkingSystem.java
@@ -0,0 +1,34 @@
+package 设计停车系统1603;
+
+public class ParkingSystem {
+
+    int big;
+    int small;
+    int medium;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public boolean addCar(int carType) {
+        if(1 == carType) {
+            boolean result = this.big > 0 ? true : false;
+            big --;
+            return result;
+        }
+        else if(2 == carType) {
+            boolean result = this.medium > 0 ? true : false;
+            this.medium --;
+            return result;
+        }
+        else if(3 == carType) {
+            boolean result = this.small > 0 ? true : false;
+            this.small --;
+            return result;
+        }
+
+        return false;
+    }
+}
