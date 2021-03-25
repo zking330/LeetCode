d2bd5899415b45e90122d1d7b38bcb5ef8535bc2
zking330 <zhengkai1995@outlook.com>
2021-03-25 17:36:06 +0800

    no.4

--- src/A数组/NO4寻找两个正序数组的中位数/Solution.java
+++ src/A数组/NO4寻找两个正序数组的中位数/Solution.java
@@ -21,26 +21,25 @@ public class Solution {
 
         if(m == 0) {
             if(n % 2 == 0) {
-                return (nums2[n/2] + nums2[n/2 -1] ) / 2;
+                return(nums2[n / 2 - 1] + nums2[n / 2]) / 2.0;
             }
             else {
-                return nums2[n/2];
+                return nums2[n / 2];
             }
         }
-
         if(n == 0) {
             if(m % 2 == 0) {
-                return (nums1[m/2] + nums1[m/2 -1] ) / 2;
+                return (nums1[m / 2 - 1] + nums1[m / 2]) / 2.0;
             }
             else {
-                return nums1[m/2];
+                return nums1[m / 2];
             }
         }
 
         int count = 0;
         int i = 0, j = 0;
 
-        while(count != m + n) {
+        while(count != (m + n)) {
             if(i == m) {
                 while(j != n) {
                     nums[count++] = nums2[j++];
