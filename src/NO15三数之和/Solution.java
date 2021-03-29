f87ab4364f4bba0cadc9530bf7c0f312a2fe007c
zking330 <zhengkai1995@outlook.com>
2021-03-29 17:51:12 +0800

    no.15三数之和

--- src/NO15三数之和/Solution.java
+++ src/NO15三数之和/Solution.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class Solution {
 
     public static void main(String[] args) {
-        int[] nums = new int[] {-1,0,1,2,-1,-4};
+        int[] nums = new int[] {-2,0,0,2,2};
         System.out.println(threeSum(nums));
     }
 
@@ -46,12 +46,12 @@ public class Solution {
                     left++; right--;
 
                     //跳过重复的值
-                    while(left < right && nums[left] == nums[left] - 1 ) {
+                    while(left < right && nums[left] == nums[left - 1]) {
                         left ++;
                     }
 
                     //跳过重复的值
-                    while(left < right && nums[right] == nums[right] + 1 ) {
+                    while(left < right && nums[right] == nums[right + 1]) {
                         right --;
                     }
                 }
